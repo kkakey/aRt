bd5bca670dfac4bde4f15489cf194639711fc0a4
kkakey <kkakey999@gmail.com>
2025-03-24 16:37:30 -0400

    Reorganzie

--- tetris1/code/tetris_plotting.R
+++ tetris1/code/tetris_plotting.R
@@ -1,17 +1,17 @@
-librarylibrary(patchwork)
-source("tetris.R")
-
-### line color
-### make some tranparent
-### what about hatching design"
-### gif: same design -- but go between hatcing and non hatching
-
 library(patchwork)
+source("tetris.R")
+library(patchwork)
+
+
+#############################################
+
+#############################################
 p1 <- tetris(background_color = F)
 p2 <- tetris(background_color = F)
 p3 <- tetris(background_color = F)
 p4 <- tetris(background_color = F)
 p5 <- tetris(background_color = F)
+
 design <- c(
   area(1, 1, 2),
   area(1, 2, 1, 3),
@@ -25,9 +25,12 @@ ok <- p1 + p2 + p3 + p4 + p5 +
     plot.background = element_rect(fill=moma.colors("OKeeffe", 10)[1], color=moma.colors("OKeeffe", 10)[1])))
 
 ggsave(plot=ok, "okeeffe_patch.png", dpi=300, height=10, width=10)
+#############################################
 
 
+#############################################
 
+#############################################
 p1 <- tetris(background_color = F, color="Koons")
 p2 <- tetris(background_color = F, color="Koons")
 p3 <- tetris(background_color = F, color="Koons")
@@ -39,9 +42,12 @@ koons <- p1 + p2 + p3 + p4 + p5 +
   plot_annotation(theme = theme(plot.margin = margin(-1, 0, -1, 0, "pt"),
     plot.background = element_rect(fill=moma.colors("Koons", 10)[1], color=moma.colors("Koons", 10)[1])))
 ggsave(plot=koons, "koons_patch.png", dpi=300, height=10, width=10)
+#############################################
 
 
+#############################################
 
+#############################################
 p1 <- tetris(background_color = F, color="Sidhu")
 p2 <- tetris(background_color = F, color="Sidhu")
 p3 <- tetris(background_color = F, color="Sidhu")
@@ -54,9 +60,11 @@ si <- p1 + p2 + p3 + p4 + p5 +
     plot.background = element_rect(fill=moma.colors("Sidhu", 10)[1], color= moma.colors("Sidhu", 10)[1])))
 
 ggsave(plot=si, "sidhu_patch.png", dpi=300, height=10, width=10)
+#############################################
 
+#############################################
 
-
+#############################################
 p1 <- tetris(background_color = F, color="Warhol")
 p2 <- tetris(background_color = F, color="Warhol")
 p3 <- tetris(background_color = F, color="Warhol")
@@ -70,14 +78,11 @@ wa <- p1 + p2 + p3 + p4 + p5 +
                                                                color = moma.colors("Warhol", 10)[1])))
 
 ggsave(plot=wa, "warhol_patch.png", dpi=300, height=10, width=10)
-
+#############################################
 
 
 all_patch <- cowplot::plot_grid(plotlist=rep(list(ok, koons, si), 3), greedy=T, ncol=3, nrow=3)
 ggsave(plot=all_patch, "all_plot1.png", width=12, height = 12)
-
-
-list(ok, koons, si, koons, ok, si, si, ok, koons)
 
 
 all_patch <- cowplot::plot_grid(plotlist=list(ok, koons, si, koons, ok, koons, si, koons, ok), greedy=T, ncol=3, nrow=3)
@@ -85,8 +90,9 @@ ggsave(plot=all_patch, "all_plot2.png", width=12, height = 12)
 
 
 
-#########
-
+#############################################
+### GIF
+#############################################
 for(i in 1:30){
   gg <- tetris(maxy = 60, maxx = 120, yrow = 25, background_color = T, color="Warhol")
   ggsave(sprintf("gif/warhol%03d.png", i), gg)
@@ -104,3 +110,5 @@ gifski(
 )
 
 file.remove(png_files)
+#############################################
+
--- tetris1/tetris_v1.R
+++ tetris1/tetris_v1.R
@@ -1,14 +1,16 @@
 ### VERSION 1
 ### play with color and sizing (maxy and maxx)
-### MAKE INTO A FUNCTION -- so you can automatically play with sizing and color --> then combine into one plot
 
 library(dplyr)
 library(ggplot2)
 # devtools::install_github("BlakeRMills/MoMAColors")
 library(MoMAColors)
 
-maxy <- 40 #850 #10
-maxx <- 100 #100 #50
+# maxy <- 40 #850 #10
+# maxx <- 100 #100 #50
+
+maxy <- 40 #10
+maxx <- 220 #100 #50
 
 start_xmin <- 0
 start_xmax <- 2
@@ -30,11 +32,11 @@ y1 <- c(y1, start_ymin)
 y2 <- c(y2, start_ymax)
 
 while (yrow < maxy) {
-  print(paste0('yrow:',yrow))
+  # print(paste0('yrow:',yrow))
 
   for (x in seq(1, maxx)) {
     
-      print(paste0('x:',x))
+      # print(paste0('x:',x))
     
       next_xmin <- max(x2)
       next_xmax <- runif(1, min=max(x2)+1, max=max(x2)+8)
@@ -64,15 +66,12 @@ plot <-
   ggplot() +
   geom_rect(data=d, mapping=aes(xmin=x1, xmax=x2, ymin=y1, ymax=y2, fill=as.factor(color)), 
             color="black") +
-  scale_fill_manual(values=moma.colors("OKeeffe", 10)[2:10]) +
+  scale_fill_manual(values=moma.colors("Flash", 10)[2:10]) +
   theme_void() +
-  theme(plot.margin = unit(c(bottom=.5, left=.5, top=.5, right=.5),"cm"),
+  theme(plot.margin = unit(c(bottom=-.5, left=-.5, top=-.5, right=-.5),"cm"),
     legend.position = "none",
-    plot.background = element_rect(fill=moma.colors("OKeeffe", 10)[1], color=NA)
+    panel.background = element_rect(linewidth=10),
+    plot.background = element_rect(fill=moma.colors("Flash", 10)[1], linewidth = 10, color="black")
   )
 
 ggsave(glue::glue('v1-{format(Sys.time(), "%Y%m%d_H%M%S")}.png'), height=6, width=6, dpi=300)
-
-
-library(patchwork)
-plot1 + plot2 + plot3 + plot4
